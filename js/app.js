f46cdc4f55e567108e151e276c37dab09d852ff2
Michael Ashabraner <ender8383@live.com>
2015-11-16 09:50:20 -0500

    namnam

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+   
+      $('.slide').slick({
+			
+			slidesToShow: 1,
+	        dots: true,
+			infinite: true,
+			speed: 500,
+			fade: true,
+			cssEase: 'linear',
+			autoplay: true,
+			centerMode: true,
+      });
+    
+
+
+
+});
